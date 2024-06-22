0de8341c520cd587e5de5f3e6e2e053fe576288c
Deo Gerard Obomby Ondaye <98679519+NeoLucem@users.noreply.github.com>
2024-06-22 16:18:22 +0000

    implemented gateway payment with flutterwave

--- app/(screens)/sender/activity/confirmPayment/[confirmationScreen].js
+++ app/(screens)/sender/activity/confirmPayment/[confirmationScreen].js
@@ -1,8 +1,36 @@
 import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react';
 import { router, useLocalSearchParams } from 'expo-router';
+import {PayWithFlutterwave} from 'flutterwave-react-native';
+import {FlutterwaveInit} from 'flutterwave-react-native';
 
 const confimationScreen = () => {
+  /* An example function to handle the redirect callback */
+  const redirectParams = {
+    status: 'successful' | 'cancelled',
+    transaction_id: "",
+    tx_ref: "",
+  };
+
+  /* An example function called when transaction is completed successfully or canceled */
+  const handleOnRedirect = (redirectParams) => {
+    console.log(redirectParams);
+  };
+
+  /* An example function to generate a random transaction reference */
+  const generateTransactionRef = (number) => {
+    var result = '';
+    var characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < number; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return `flw_tx_ref_${result}`;
+  };
+
+  
+
   const { 
     senderId,
     receiver_name,
@@ -22,6 +50,7 @@ const confimationScreen = () => {
     request_ID,
     sender_name,
     sender_budget,);
+
   return (
     <SafeAreaView className="h-full justify-center items-center">
       <View>
@@ -44,6 +73,20 @@ const confimationScreen = () => {
                 <Text className="text-center">Pay !</Text>
             </TouchableOpacity>
         </View>
+        <PayWithFlutterwave
+          onRedirect={handleOnRedirect}
+          options={{
+            tx_ref: generateTransactionRef(10),
+            authorization: 'FLWPUBK_TEST-9d6bf188b8fbf4f53aa6934ac88ebe34-X',
+            customer: {
+              email: 'customer-email@example.com'
+            },
+            amount: 1,
+            currency: 'GHS',
+            payment_options: 'card',
+            redirect_url: 'https://neolucem.github.io'
+          }}
+        />
       </View>
     </SafeAreaView>
   )
